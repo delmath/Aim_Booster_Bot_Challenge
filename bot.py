3fd8628bbccbb33c1a09520b18ed2116502fd395
delmath <delvincourtmathis@gmail.com>
2023-05-10 23:34:42 +0200

    add bot base

---
+++ bot.py
@@ -0,0 +1,42 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import win32api, win32con
+
+print('Cliquer sur q pour arrêter le bot')
+print('Veuillez cliquer dans le coin supérieur de la région de capture... (a pour valider)')
+
+while True:
+    if keyboard.is_pressed('q'):
+        quit()
+    if keyboard.is_pressed('a'):
+        x1, y1 = win32api.GetCursorPos()
+        break
+
+print('Angle supérieur OK !')
+time.sleep(1)
+print('Veuillez cliquer dans le coin inférieur de la région de capture... (a pour valider)')
+
+while True:
+    if keyboard.is_pressed('q'):
+        quit()
+    if keyboard.is_pressed('a'):
+        x2, y2 = win32api.GetCursorPos()
+        break
+
+print('Angle inférieur OK!')
+print('Bot en cours d\'exécution dans 2s')
+time.sleep(2)
+
+while keyboard.is_pressed('q') == False:
+    pic = pyautogui.screenshot(region=(x1, y1, x2, y2))
+    for x in range(0, pic.width, 5):
+        for y in range(0, pic.height, 5):
+            if pic.getpixel((x, y)) == (255, 219, 195):
+                click(x+x1, y+y1)
+                time.sleep(0.05)
+                break
+        else:
+            continue
+        break
